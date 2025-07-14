f13c2835b0df20e40c25622b814ac04f20c28258
sergey.alekseev <demiurg58@gmail.com>
2025-07-14 21:34:27 +0300

    test pipe

--- vars/fastApi.groovy
+++ vars/fastApi.groovy
@@ -1,6 +1,8 @@
-def call ()
+def call () {
 
     node("Builder") {
         def webhookPayload = readJSON text: env.JSON_PAYLOAD;
-        def webhookQuery = readJSON text: env.JSON_QUERY;
+
+        echo "${webhookPayload}"
     }
+}
