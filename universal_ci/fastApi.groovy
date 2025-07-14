913acfc5fde17ace0969974861af8c6f2bad3f9a
sergey.alekseev <demiurg58@gmail.com>
2025-07-14 20:49:55 +0300

    test pipe

--- universal_ci/fastApi.groovy
+++ universal_ci/fastApi.groovy
@@ -1,1 +1,17 @@
-// some pipeline 
+node {
+    stage('Clone FastAPI Repo') {
+        def repoUrl = 'git@github.com:your-user/devops-wiki-fastapi.git'
+        def branch = 'main'
+
+        echo "Cloning ${repoUrl} (branch: ${branch})"
+        // Очищаем рабочую директорию перед сборкой
+        deleteDir()
+        // Клонируем репозиторий
+        git branch: branch, url: repoUrl
+    }
+
+    stage('List Files') {
+        echo "Project directory contents:"
+        sh 'ls -la'
+    }
+}
