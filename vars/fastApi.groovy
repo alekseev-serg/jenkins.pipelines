0c0ff502350cef99d99fd12f312816c7a106bf2c
sergey.alekseev <demiurg58@gmail.com>
2025-07-14 22:03:13 +0300

    test pipe

--- vars/fastApi.groovy
+++ vars/fastApi.groovy
@@ -6,10 +6,12 @@ def call () {
 
         echo "RAW JSON: ${env.JSON_PAYLOAD ?: 'JSON_PAYLOAD is null'}"
 
-        if (env.JSON_PAYLOAD) {
-            def webhookPayload = new JsonSlurper().parseText(env.JSON_PAYLOAD)
-        } 
+        def webhookPayload = new JsonSlurper().parseText(env.JSON_PAYLOAD)
 
         def context = init(webhookPayload)
+
+        echo "${context.gitUrl}"
+        echo "${context.branch}"
+        echo "${context.appName}"
     }
 }
