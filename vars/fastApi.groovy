dd8d8072424d0b315e399e061b56b9a93811d8d2
sergey.alekseev <demiurg58@gmail.com>
2025-07-15 16:46:47 +0300

    test pipe

--- vars/fastApi.groovy
+++ vars/fastApi.groovy
@@ -3,25 +3,21 @@ import groovy.json.JsonSlurper
 def call () {
 
     node("builder") {
-        sh "env"
 
         echo "RAW JSON: ${env.JSON_PAYLOAD ?: 'JSON_PAYLOAD is null'}"
 
         def webhookPayload = readJSON text: env.JSON_PAYLOAD
 
+        def context = init(webhookPayload)
 
-        def gitUrl = webhookPayload.repository?.ssh_url
-        def branch = webhookPayload.ref.tokenize('/').last()
-        def appName = webhookPayload.repository?.name
-
-        echo "${gitUrl} ${branch} ${appName}"
+        echo "${context.gitUrl} ${context.branch} ${context.appName}"
 
         stage('Get source code') {
             checkout([
                 $class: 'GitSCM',
-                branches: [[name: branch]],
+                branches: [[name: context.branch]],
                 userRemoteConfigs: [[
-                    url: gitUrl,
+                    url: context.gitUrl,
                     credentialsId: 'git-ssh'
                 ]],
             ]);
